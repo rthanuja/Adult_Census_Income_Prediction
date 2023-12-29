2590b096911b078732aba96b03cf7b6ff58ab320
Thanuja Ramisetty <rthanuja.ramisetty@gmail.com>
2023-12-29 19:12:05 +0530

    setupfile added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+setup(
+    name='IncomePrediction',
+    version='0.0.1',
+    author='r thanuja',
+    author_email='rthanuja.ramisetty@gmail.com',
+    packages=find_packages()
+)
