e4276dae74b6d1592688c8d1633447f00cf16c35
Thanuja Ramisetty <rthanuja.ramisetty@gmail.com>
2024-02-12 13:13:31 +0530

    updated

---
+++ app.py
@@ -0,0 +1,55 @@
+from wsgiref import simple_server
+from flask import Flask, request, render_template
+from flask import Response
+from flask_cors import CORS, cross_origin
+import Flask_MonitoringDashboard as dashboard
+import os
+
+from src.IncomePrediction.validation_Insertion.training_validation_insertion import train_validation
+from src.IncomePrediction.pipelines.training_pipeline import TrainModel
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
+app = Flask(__name__)
+dashboard.bind(app)
+CORS(app)
+
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('index.html')
+
+
+@app.route("/train", methods=['POST'])
+@cross_origin()
+def trainRouteClient():
+
+    try:
+        if request.json['folderPath'] is not None:
+            path = request.json['folderPath']
+            train_valObj = train_validation() #object initialization
+
+            train_valObj.train_validation()#calling the training_validation function
+
+
+            trainModelObj = TrainModel() #object initialization
+            trainModelObj.train_model() #training the model for the files in the table
+
+
+    except ValueError:
+
+        return Response("Error Occurred! %s" % ValueError)
+
+    except KeyError:
+
+        return Response("Error Occurred! %s" % KeyError)
+
+    except Exception as e:
+
+        return Response("Error Occurred! %s" % e)
+    return Response("Training successfull!!")
+
+if __name__==  "__main__":
+    app.run(host="0.0.0.0",port=8080)
--- src/IncomePrediction/pipelines/training_pipeline.py
+++ src/IncomePrediction/pipelines/training_pipeline.py
@@ -10,42 +10,46 @@ import sys
 import os
 import pandas as pd
 
-data_ingestion_obj = Data_Ingestion()
+class TrainModel:
+    
+    def train_model(self):    
+        logging.info("Start of Training!")  
+        try:    
+            data_ingestion_obj = Data_Ingestion()
+            
+            data = data_ingestion_obj.initiate_data_ingestion()
 
-data = data_ingestion_obj.initiate_data_ingestion()
+            data_tranformation_obj = DataTranformation()
 
-data_tranformation_obj = DataTranformation()
+            X= data_tranformation_obj.initialize_data_transformation(data)
 
-X= data_tranformation_obj.initialize_data_transformation(data)
+            list_of_clusters = X['cluster'].unique()
 
-list_of_clusters = X['cluster'].unique()
 
-try:
+            for i in list_of_clusters:
+                cluster_data= X[X['cluster']==i] # filter the data for one cluster
+                # Prepare the feature and Label columns
+                cluster_features= cluster_data.drop(['Labels','cluster'],axis=1)
+                cluster_label= cluster_data['Labels']
 
-    for i in list_of_clusters:
-        cluster_data= X[X['cluster']==i] # filter the data for one cluster
-        # Prepare the feature and Label columns
-        cluster_features= cluster_data.drop(['Labels','cluster'],axis=1)
-        cluster_label= cluster_data['Labels']
+                # splitting the data into training and test set for each cluster one by one
+                x_train, x_test, y_train, y_test = train_test_split(cluster_features, cluster_label, test_size=1 / 3, random_state=355)
 
-        # splitting the data into training and test set for each cluster one by one
-        x_train, x_test, y_train, y_test = train_test_split(cluster_features, cluster_label, test_size=1 / 3, random_state=355)
+                model_finder = Model_finder() # object initialization
 
-        model_finder = Model_finder() # object initialization
+                #getting the best model for each of the clusters
+                
+                best_model_name,best_model = model_finder.get_best_model(x_train,y_train,x_test,y_test)
 
-        #getting the best model for each of the clusters
-        
-        best_model_name,best_model = model_finder.get_best_model(x_train,y_train,x_test,y_test)
+                file_obj = File_Operation()
+                filename = best_model_name+str(i)
+                file_obj.save_model(file_path = os.path.join('artifacts',filename),
+                                    obj = best_model)
+                logging.info("successful end of training")
 
-        file_obj = File_Operation()
-        filename = best_model_name+str(i)
-        file_obj.save_model(file_path = os.path.join('artifacts',filename),
-                            obj = best_model)
-        logging.info("successful end of training")
-        
-except Exception as e:
-    logging.info("error occcured in training pipeline")
-    raise customException(e,sys)
+        except Exception as e:
+            logging.info("error occcured in training pipeline")
+            raise customException(e,sys)
 
     
 
